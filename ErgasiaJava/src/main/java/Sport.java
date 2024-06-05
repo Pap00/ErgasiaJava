3b7bd5ddc1bf8485859a5ce909d346b65685e097
chriskourtis <christoskourths@gmail.com>
2024-06-05 21:46:18 +0300

    Update Sport.java

--- ErgasiaJava/src/main/java/Sport.java
+++ ErgasiaJava/src/main/java/Sport.java
@@ -1,4 +1,28 @@
 
 public class Sport {
 
+	private String sportName; 
+	private String levelRef; //variable to determine if 
+                            //the sport refers to professionals
+	
+	Sport(String sportName,String levelRef ){
+	
+		this.sportName = sportName;
+		this.levelReference = levelRef;
+	}
+	
+	String getSportName() {
+		return sportName;
+	}
+	public void setSportName() {
+		this.sportName = sportName;
+	}
+	String levelRef() {
+		return levelRef;
+	}
+	public void levelRef() {
+		this.levelRef = levelRef;
+	}
+
+
 }

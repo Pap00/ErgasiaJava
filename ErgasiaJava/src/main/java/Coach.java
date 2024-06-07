df6d3f6fdb3936eddb01d9605a2a00ae0d97ec6c
Pap00 <88512643+Pap00@users.noreply.github.com>
2024-06-07 11:40:21 +0300

    Update Coach.java

--- ErgasiaJava/src/main/java/Coach.java
+++ ErgasiaJava/src/main/java/Coach.java
@@ -1,8 +1,24 @@
 
 public class Coach extends User {
-	private int degrees;
-	private char Sport;
+	private String sport;
+	private String degree;
 	
-
+	Coach(String name, int age, char gender,String surname,String contactinfo,String sport,String degree){
+		super(name,age,gender,surname,contactinfo);
+		this.sport=sport;
+		this.degree=degree;
+	}
+	void setSport(String sport) {
+		this.sport=sport;
+	}
+	void setDegree(String degree) {
+		this.degree=degree;
+	}
+	String getSport() {
+		return sport;
+	}
+	String getDegree() {
+		return degree;
+	}
 }
 

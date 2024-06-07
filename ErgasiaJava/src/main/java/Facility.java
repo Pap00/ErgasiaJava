9b405c36d342864560ef91b05b1051f9dfe7eadc
Pap00 <88512643+Pap00@users.noreply.github.com>
2024-06-07 11:41:03 +0300

    Update Facility.java

--- ErgasiaJava/src/main/java/Facility.java
+++ ErgasiaJava/src/main/java/Facility.java
@@ -1,4 +1,21 @@
-
 public class Facility {
-
+	private String name;
+	private int capacity;
+	
+	Facility(String name, int capacity){
+		this.name=name;
+		this.capacity=capacity;
+	}
+	String getName() {
+		return name;
+	}
+	int getCapacity() {
+		return capacity;
+	}
+	void setName(String name) {
+		this.name=name;
+	}
+	void setCapacity(int capacity) {
+		this.capacity=capacity;
+	}
 }

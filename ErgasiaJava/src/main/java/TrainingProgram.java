45da0ea054424ff8d9af660dd1c3728bb1d9d5e4
cretsas <152790714+cretsas@users.noreply.github.com>
2024-06-08 17:05:20 +0300

    Update TrainingProgram.java

--- ErgasiaJava/src/main/java/TrainingProgram.java
+++ ErgasiaJava/src/main/java/TrainingProgram.java
@@ -1,4 +1,28 @@
+import java.time.DayOfWeek;
 
 public class TrainingProgram {
+    private static int counter = 0;
+    private int id;
+    private Sport sport;
+    private Facility facility;
+    private Coach coach;
+    private int minExperienceLevel;
+    private boolean requiresWeeklyBooking;
+    private String participantGender;
+    private int durationInMinutes;
+    private DayOfWeek dayOfWeek;
 
+    public TrainingProgram(Sport sport, Facility facility, Coach coach, int minExperienceLevel, boolean requiresWeeklyBooking, String participantGender, int durationInMinutes, DayOfWeek dayOfWeek) {
+        this.id = ++counter;
+        this.sport = sport;
+        this.facility = facility;
+        this.coach = coach;
+        this.minExperienceLevel = minExperienceLevel;
+        this.requiresWeeklyBooking = requiresWeeklyBooking;
+        this.participantGender = participantGender;
+        this.durationInMinutes = durationInMinutes;
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    // getters and setters
 }

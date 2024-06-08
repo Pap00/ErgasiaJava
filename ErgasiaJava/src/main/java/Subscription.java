a37b125b1cf904b958160aacee12da9c8f2ee608
cretsas <152790714+cretsas@users.noreply.github.com>
2024-06-08 17:04:26 +0300

    Update Subscription.java

--- ErgasiaJava/src/main/java/Subscription.java
+++ ErgasiaJava/src/main/java/Subscription.java
@@ -1,4 +1,21 @@
+public class Subscription implements Pricelist {
+    private static int counter = 0;
+    private int id;
+    private Athlete athlete;
+    private TrainingProgram trainingProgram;
+    private double monthlyCost;
 
-public class Subscription {
+    public Subscription(Athlete athlete, TrainingProgram trainingProgram, double monthlyCost) {
+        this.id = ++counter;
+        this.athlete = athlete;
+        this.trainingProgram = trainingProgram;
+        this.monthlyCost = monthlyCost;
+    }
 
+    @Override
+    public double calculateTotalPrice() {
+        return monthlyCost;
+    }
+
+    // getters and setters
 }

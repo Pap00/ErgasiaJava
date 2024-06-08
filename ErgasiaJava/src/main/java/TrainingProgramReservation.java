843f226363c80c7ffae5e0a7e4e0e5e494c57f79
cretsas <152790714+cretsas@users.noreply.github.com>
2024-06-08 17:04:01 +0300

    Update TrainingProgramReservation.java

--- ErgasiaJava/src/main/java/TrainingProgramReservation.java
+++ ErgasiaJava/src/main/java/TrainingProgramReservation.java
@@ -1,4 +1,18 @@
+import java.time.LocalDate;
 
 public class TrainingProgramReservation {
+    private static int counter = 0;
+    private int id;
+    private Athlete athlete;
+    private TrainingProgram trainingProgram;
+    private LocalDate trainingDate;
 
+    public TrainingProgramReservation(Athlete athlete, TrainingProgram trainingProgram, LocalDate trainingDate) {
+        this.id = ++counter;
+        this.athlete = athlete;
+        this.trainingProgram = trainingProgram;
+        this.trainingDate = trainingDate;
+    }
+
+    // getters and setters	
 }

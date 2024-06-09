275e01cf2b6588ab4ba3d595916bdc28b8f5ffc9
chriskourtis <christoskourths@gmail.com>
2024-06-09 17:07:48 +0300

    Update InterfacePriceList.java

--- ErgasiaJava/src/main/java/InterfacePriceList.java
+++ ErgasiaJava/src/main/java/InterfacePriceList.java
@@ -1,4 +1,23 @@
 
 public class InterfacePriceList {
 
+	
+	private long int charges;
+	
+	InterfacePriceList(){
+		this.charges charges;
+	}
+	
+	long int getCharges() {
+		return charges;
+	}
+	public void setCharges() {
+		this.charges = charges;
+	}
+
+	public double calculateTotalPrice(){
+		
+		return totalPrice;
+	}
+	
 }

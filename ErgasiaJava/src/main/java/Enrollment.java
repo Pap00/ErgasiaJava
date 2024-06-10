59beb0f9c1bbc59372dcd796adf21a103ba9badb
Pap00 <88512643+Pap00@users.noreply.github.com>
2024-06-10 12:39:15 +0300

    Update Enrollment.java

--- ErgasiaJava/src/main/java/Enrollment.java
+++ ErgasiaJava/src/main/java/Enrollment.java
@@ -1,4 +1,22 @@
-
-public class Enrollment {
-
+public class Enrollment implements InterfacePriceList{
+	private int code;
+	private String athlete;
+	private String date;
+	private int cost;
+	private int sale;
+	
+	Enrollment(int code, String athlete, String date, int cost, int sale){
+		this.code=code;
+		this.athlete=athlete;
+		this.date=date;
+		this.cost=cost;
+		this.sale=sale;				
+	}
+	
+	public String calculateTotalPrice() {		
+		return null;
+	}
+	
+	
+	
 }

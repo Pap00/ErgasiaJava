7f5b3fabdfe050d3ecaff6d4b2990ac565504424
cretsas <152790714+cretsas@users.noreply.github.com>
2024-06-11 15:27:22 +0300

    Update Sport.java

--- ErgasiaJava/src/main/java/Sport.java
+++ ErgasiaJava/src/main/java/Sport.java
@@ -1,28 +1,59 @@
 
-public class Sport {
+public class Payment implements Pricelist{
 
-	private String sportName; 
-	private String levelRef; //variable to determine if 
-                            //the sport refers to professionals
+	private String uniqueCode;
+	private String date;
+	private String methodOfPayment;
+	private String regOrSig; //register or signup
+	private long int totalPrice;
 	
-	Sport(String sportName,String levelRef ){
-	
-		this.sportName = sportName;
-		this.levelReference = levelRef;
+	Payment(String uniqueCode,String date,String methodOfPayment,String regOrSig,long int totalPrice){
+		this.uniqueCode= uniqueCode;
+		this.date = date;
+		this.methodOfPayment = methodOfPayment;
+		this.regOrSig = regOrSig;
+		this.totalPrice = totalPrice;
 	}
 	
-	String getSportName() {
-		return sportName;
+	
+	
+	String getUniqueCode() {
+		return uniqueCode;
 	}
-	public void setSportName(String sportName) {
-		this.sportName = sportName;
+	
+	public void setUniqueCode(String uniqueCode) {
+		this.uniqueCode = uniqueCode;
 	}
-	String getLevelRef() {
-		return levelRef;
+	
+	String getDate() {
+		return date;
 	}
-	public void setLevelRef(String levelRef) {
-		this.levelRef = levelRef;
+	
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	String getMethodOfPayment() {
+		return methodOfPayment;
+	}
+	public void setMethodOfPayment(String methodOfPayment) {
+		this.methodOfPayment = methodOfPayment;
 	}
 
-
+	String getRegOrSig() {
+		return RegOrSig;
+	}
+	
+	public void setRegOrSig(String regOrSig) {
+		this.RegOrSig= RegOrSig;
+	}
+	
+	long int getTotalPrice() {
+		return totalPrice;
+	}
+	
+	public void setTotalPrice(long int totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+	
 }

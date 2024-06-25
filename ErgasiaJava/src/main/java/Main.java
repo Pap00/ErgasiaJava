3b103b94cfea2aee234f106561411575a8a0a9e5
cretsas <152790714+cretsas@users.noreply.github.com>
2024-06-25 12:05:41 +0300

    Update Main.java

--- ErgasiaJava/src/main/java/Main.java
+++ ErgasiaJava/src/main/java/Main.java
@@ -1,9 +1,45 @@
+package lab.unipi.gui.JavaFXLab;
 
-public class Main {
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		System.out.println("o giannis einai vlakas");
 
-	}
+
+public class App extends Application {
+
+	Stage primaryStage;
+	Scene mainScene, playerScene;
+	FlowPane rootFlowPane;
+	Button plrbtn,subbtn,resbtn;
+	public void start(Stage primaryStage) {
+		this.primaryStage=primaryStage;
+		rootFlowPane=new FlowPane();
+		plrbtn = new Button("Διαχειρηση Αθλητων");
+		subbtn = new Button("Διαχειρηση Συνδρομων");
+		resbtn = new Button("Διαχειρηση Κρατησεων");
+		
+		rootFlowPane.setHgap(10);
+		rootFlowPane.setAlignment(Pos.CENTER);
+		
+		rootFlowPane.getChildren().add(plrbtn);
+		rootFlowPane.getChildren().add(subbtn);
+		rootFlowPane.getChildren().add(resbtn);
+		
+		mainScene = new Scene(rootFlowPane, 650, 300);
+		
+		primaryStage.setScene(mainScene);
+		primaryStage.setTitle("Διαχείρηση Αθλητικών Ακαδημίων");
+		primaryStage.show();
+		}
+	
+    public static void main(String[] args) {
+        launch();
+    }
 
 }
